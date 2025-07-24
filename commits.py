6c86d7798137f5ebf5d1d1269e30d4ac61cbd107
Lucas <lubertanhe@gmail.com>
2025-07-23 23:18:18 -0300

    Feat: DevOps Test Two

--- commits.py
+++ commits.py
@@ -1,2 +1,12 @@
-input ('Salve' "Teste Salvamento DevOPS")
-int(input('Digite um número: '))
+x = input ("Teste Salvamento DevOPS: Digite Inial DevOPS: ")
+y = int(input('Digite um catalogação de DevOPS: ')) 
+print(f"Você digitou: {x} e {y}")
+print(f"(x, y) = ({x}, {y})")
+z = x + str(y)
+print(f"z = {z}")
+print(f"z = {z} e o tipo é {type(z)}")
+print(f"Z é a catalogação de DevOPS: {z}")
+z + y + t = 10
+print(f"Z é a catalogação de DevOPS: {z} e o tipo é {type(z)}")
+t = 10
+print(f"Z é a catalogação de DevOPS: {z} e o tipo é
